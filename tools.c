8b913e493b8f55c11b7b77a5a9a8b4af113c4dc9
Your Name <you@example.com>
2016-03-06 21:44:58 +0100

    autopush

--- tools.c
+++ tools.c
@@ -42,3 +42,37 @@ void    pb(t_pile *pile_a, t_pile *pile_b)
     pile_b->beg = tmp;
 }
 
+void    pa(t_pile *pile_a, t_pile *pile_b)
+{
+    t_node  *tmp;
+    tmp = pile_b->beg;
+    pile_b->beg = pile_b->beg->next;
+    tmp->next = pile_a->beg;
+    pile_a->beg = tmp;
+}
+
+void    ra(t_pile *pile_a)
+{
+    t_node  *tmp;
+    tmp = pile_a->beg;
+    pile_a->beg = pile_a->beg->next;
+    pile_a->beg->prev = NULL;
+    pile_a->last->next = tmp;
+    pile_a->last = tmp;
+}
+
+void    rb(t_pile *pile_b)
+{
+    t_node  *tmp;
+    tmp = pile_b->beg;
+    pile_b->beg = pile_b->beg->next;
+    pile_b->beg->prev = NULL;
+    pile_b->last->next = tmp;
+    pile_b->last = tmp;
+}
+
+void    rr(t_pile *pile_a, t_pile *pile_b)
+{
+    ra(pile_a);
+    rb(pile_b);
+}
