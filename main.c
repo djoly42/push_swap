d8f03dc9f0a3a7c76f44c664367ce35b7167b274
Your Name <you@example.com>
2016-03-06 13:40:46 +0100

    autopush

--- main.c
+++ main.c
@@ -1,15 +1,28 @@
 
 #include "push_swap.h"
 
+void    init_a(t_env *env, char *av, int nb)
+{
+    int i;
+    t_node = tmp;
+    t_node =
+    a_size = nb;
+    i = 0;
+    do
+    {
+     tmp = (t_node*)malloc(sizeof(t_node));
+     tmp->prev = NULL;
+
+
+}
+
 int     main(int argc, char *argv)
 {
     t_env   env;
-    t_node  *a;
-    t_node  *b;
     
     if (argc < 2)
         return (0);
-    init_a(a, argv, (argc - 1));
+    init_a(&env, argv, (argc - 1));
 
 
     return (0);
