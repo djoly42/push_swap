cbe5fd3aaf269078f881c0915c1866f63ac94a8e
Your Name <you@example.com>
2016-03-17 23:15:33 +0100

    autopush

--- solve.c
+++ solve.c
@@ -12,7 +12,6 @@
 
 #include "Includes/push_swap.h"
 
-
 int		chr_forward(int data, t_node *node)
 {
 	int	i;
@@ -42,6 +41,19 @@ int		chr_back(int data, t_node *node)
 	}
 	return (i);
 }
+
+int		which_behavior(int f[])
+{
+	if (f[0] < f[1] && f[0] < f[2] && f[0] < f[3])
+		return (0);
+	if (f[0] > f[1] && f[1] < f[2] && f[1] < f[3])
+		return (1);
+	if (f[2] < f[1] && f[0] > f[2] && f[2] < f[3])
+		return (2);
+	if (f[3] < f[1] && f[3] < f[2] && f[0] > f[3])
+		return (3);
+}
+
 int		solve(t_pile *pile_a, t_pile *pile_b)
 {
 	int	f[4];
@@ -53,5 +65,5 @@ int		solve(t_pile *pile_a, t_pile *pile_b)
 		f[2] = chr_forward(pile_a->beg->next->data, pile_b->beg);
 		f[3] = chr_back(pile_a->beg->next->data, pile_b->last);
 	}
-	return (wich_behavior(f[]));
+	push(pile_a, pile_b, which_behavior(f[]));
 }
